18d7a98dae1dcd483863a063ce82c98dc694e389
Kyle <kylemackie2012@gmail.com>
2025-10-16 14:04:11 +1000

    Created layer normalization often used in Transformers

--- recognition/ConvNeXt-48035657/modules.py
+++ recognition/ConvNeXt-48035657/modules.py
@@ -2,3 +2,21 @@
 Neural Network Modules for Alzheimer's Disease Classification
 Using ConvNeXt based architecture for brain MRI classification
 """
+
+import torch
+import torch.nn as nn
+
+class LayerNorm2d(nn.Module):
+    """2D Layer Normalization for ConvNeXt blocks"""
+    def __init__(self, num_channels, eps=1e-6):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(num_channels))
+        self.bias = nn.Parameter(torch.zeros(num_channels))
+        self.eps = eps
+
+    def forward(self, x):
+        u = x.mean(1, keepdim=True)
+        s = (x - u).pow(2).mean(1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.eps)
+        x = self.weight[:, None, None] * x + self.bias[:, None, None]
+        return x
