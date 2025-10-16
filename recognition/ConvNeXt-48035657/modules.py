410b5359f39259b0ba4bcba4f5167cc200dd3fe3
Kyle <kylemackie2012@gmail.com>
2025-10-16 14:15:28 +1000

    Added DropPath modules to prevent overfitting

--- recognition/ConvNeXt-48035657/modules.py
+++ recognition/ConvNeXt-48035657/modules.py
@@ -6,7 +6,7 @@ Using ConvNeXt based architecture for brain MRI classification
 import torch
 import torch.nn as nn
 
-class LayerNorm2d(nn.Module):
+class LayerNorm2D(nn.Module):
     """2D Layer Normalization for ConvNeXt blocks"""
     def __init__(self, num_channels, eps=1e-6):
         super().__init__()
@@ -20,3 +20,23 @@ class LayerNorm2d(nn.Module):
         x = (x - u) / torch.sqrt(s + self.eps)
         x = self.weight[:, None, None] * x + self.bias[:, None, None]
         return x
+    
+class DropPath(nn.Module):
+    """
+    Drop entire samples from path to prevent overfitting
+    Samples are dropped entirely with drop_prob chance    
+    """
+    def __init__(self, drop_prob=0.0):
+        super().__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        if self.drop_prob == 0.0 or not self.training:
+            return x
+        keep_prob = 1 - self.drop_prob
+        shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+        random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+        # Sets tensor to all 0s or all 1s with drop_prob or keep_prob chance respectively
+        random_tensor.floor_()
+        output = x.div(keep_prob) * random_tensor
+        return output
